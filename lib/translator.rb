9ed9ea11b0ba3e322cc1c845713cb1fd39f68d29
Roman Pirag <mr.anadale@yahoo.com>
2018-11-18 19:04:31 +0000

    automatically backed up by learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -2,7 +2,9 @@ require 'yaml'
 
 def load_library(file_path)
   emoticons_list = YAML.load_file(file_path)
-  emoticons_list.each do |meaning,  
+  emoticons_list.each do |meaning, language|
+    return get_meaning = meaning 
+  end
 end
 
 def get_japanese_emoticon
